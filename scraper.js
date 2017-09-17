14548656ced5873ad5a58cbd6dc92b6fbdb43cc2
user <user@lab1550.tumo.lab>
2017-09-17 13:28:01 +0400

    Porc

--- scraper.js
+++ scraper.js
@@ -4,10 +4,10 @@ var jsonfile = require('jsonfile')
 
 var file = 'data1.json'
 
-scrapeIt("https://www.sas.am/", {
+scrapeIt("https://www.list.am/", {
     // Fetch the articles
     articles: {
-        listItem: ".table"
+        listItem: ".td-overally" //"table.table tbody"
       , data: {
             // Get the article date and convert it into a Date object
         //     createdAt: {
@@ -20,17 +20,15 @@ scrapeIt("https://www.sas.am/", {
         //       , how: "html"
         //     }
             content: {
-                selector
+                selector: "h3", // #comp_532cdfe694f5c64e7122aec4c3440909 > div > table > tbody > tr > td
+                how: "html"
             }
         }
     }
 
     // Fetch some other data from the page
-  , title: ".overflow h1"
-  , avatar: {
-        selector: ".logo img"
-      , attr: "src"
-    }
+  , title: ".t"//body > div.main.index > div.outer > div.container > div > div.promo > div > h2 > a
+
 }, (err, page) => {
     console.log(err || page);
      jsonfile.writeFile(file, page,  {flag: 'a', spaces: 2}, function(err) {
